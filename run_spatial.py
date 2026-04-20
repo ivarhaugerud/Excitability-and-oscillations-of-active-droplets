13f5321d1de4f22a19180a4b644efefe89a69310
Ivar Haugerud <ivar.haugerud@gmail.com>
2026-04-20 21:48:19 +0200

    quick update

--- run_spatial.py
+++ run_spatial.py
@@ -21,30 +21,28 @@ def non_linear_terms(A_tilde, B_tilde, delta, thresh, steep, fuel, k_p, omegaA, 
     B = irfft2(B_tilde)
 
     fuel_vals = fuel_foo(A, B, thresh, steep, fuel)
+
     S = 1-A-B
-    S_tilde = rfft2(S)
     
     # get mobilities
-    A_mobfac = A
-    B_mobfac = B
-    C_mobfac = S
+    A_mobfac = A*S
+    B_mobfac = B*S
 
     # chemical potentials (without surface tension) (in real space)
     Lambda = 1 + (chi_AB*A*B + chi_AS*A*S + chi_BS*B*S)
-    df0dA = np.log(A) + 1 + chi_AB*B + chi_AS*S + omegaA - Lambda + U*A
-    df0dB = np.log(B) + 1 + chi_AB*A + chi_BS*S + omegaB - Lambda + U*B
-    df0dC = np.log(S) + 1 + chi_AS*A + chi_BS*B + omegaS - Lambda #+ U*S
+    df0dA = np.log(A) + 1 + chi_AB*B + chi_AS*S + omegaA - Lambda + U
+    df0dB = np.log(B) + 1 + chi_AB*A + chi_BS*S + omegaB - Lambda + U
+    mu_C  = np.log(S) + 1 + chi_AS*A + chi_BS*B + omegaS - Lambda 
 
     # surface tension chemical contribution (in real space)
     gradA = irfft2(Lambda_A*k2*A_tilde)
     gradB = irfft2(Lambda_B*k2*B_tilde)
 
-    mu_A = df0dA + gradA
-    mu_B = df0dB + gradB
-    mu_C = df0dC
+    mu_A = df0dA + gradA 
+    mu_B = df0dB + gradB 
 
     #laplacians becomes k-vectors, here you you calculate spatial derivative of mu in k-space
-    chem_pot_A_x = irfft2(1j*k_x*rfft2(mu_A - mu_C)) 
+    chem_pot_A_x = irfft2(1j*k_x*rfft2(mu_A - mu_C))
     chem_pot_A_y = irfft2(1j*k_y*rfft2(mu_A - mu_C))
     chem_pot_B_x = irfft2(1j*k_x*rfft2(mu_B - mu_C))
     chem_pot_B_y = irfft2(1j*k_y*rfft2(mu_B - mu_C))
@@ -55,12 +53,10 @@ def non_linear_terms(A_tilde, B_tilde, delta, thresh, steep, fuel, k_p, omegaA, 
     vec_x_B = rfft2(rho_B*Gamma_B*B_mobfac*chem_pot_B_x)
     vec_y_B = rfft2(rho_B*Gamma_B*B_mobfac*chem_pot_B_y)
    
-    r1  = rfft2((np.exp(mu_A + fuel_vals)  - np.exp(mu_B)))
+    r1  = rfft2((np.exp(mu_A + fuel_vals)  - np.exp(mu_B)),)
     r2  = rfft2(k_p*(np.exp(mu_A + mu_B)  - np.exp(2*mu_C)))
 
-    #divergence of mobility times chemical potential gradient, and noise
     return (1j*k_x*vec_x_A + 1j*k_y*vec_y_A  - r1 - r2, 1j*k_x*vec_x_B + 1j*k_y*vec_y_B  + r1 - r2)
-    
 
 def implicitrk(u, v, delta, thresh, steep, fuel, k_p, omegaA, omegaB, omegaC, rho_A, rho_B, n_step, Gamma_A, Gamma_B, Lambda_A, Lambda_B, k_x, k_y, k2, U, u_val, l):
     """
@@ -81,7 +77,7 @@ def implicitrk(u, v, delta, thresh, steep, fuel, k_p, omegaA, omegaB, omegaC, rh
             u_tilde = irfft2(u)
             v_tilde = irfft2(v)
 
-            print(niter, np.mean(u_tilde), np.mean(v_tilde), np.mean(u_tilde+v_tilde))
+            print(niter*delta, np.mean(u_tilde), np.mean(v_tilde), np.mean(u_tilde+v_tilde), np.amax(u_tilde), np.amax(v_tilde), np.amax(u_tilde+v_tilde))
             np.save("data/phiA_fuel%3.2f_delta%3.5f_GammaA%3.5f_GammaB%3.5f_Lambda%3.5f_Lambda%3.5f_chiAB%3.3f_chiAC%3.3f_chiBC%3.3f_phiA%3.3f_phiB%3.3f_uval%3.2f_l%3.2f_time%3.6f" % (fuel, delta, Gamma_A, Gamma_B, Lambda_A, Lambda_B, chi_AB, chi_AS, chi_BS, Atot, Btot, u_val, l, niter*delta) + ".npy", u_tilde)
             np.save("data/phiB_fuel%3.2f_delta%3.5f_GammaA%3.5f_GammaB%3.5f_Lambda%3.5f_Lambda%3.5f_chiAB%3.3f_chiAC%3.3f_chiBC%3.3f_phiA%3.3f_phiB%3.3f_uval%3.2f_l%3.2f_time%3.6f" % (fuel, delta, Gamma_A, Gamma_B, Lambda_A, Lambda_B, chi_AB, chi_AS, chi_BS, Atot, Btot, u_val, l, niter*delta) + ".npy", v_tilde)
         
